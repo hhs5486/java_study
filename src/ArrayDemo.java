5b3a6e84ac3f6afe4cf9e3127274bd7f8ff7b966
first_commit <hhs5486@naver.com>
2025-02-25 14:39:46 +0900

    변경

---
+++ src/ArrayDemo.java
@@ -0,0 +1,59 @@
+public class ArrayDemo {
+  public static void main(String[] args) {
+
+
+//    int score1 = 100, score2 =90, score3=80, score4=90, score5=90, score6=70;
+//    int total = score1+score2+score3+score4+score5;
+//    double avg = total/(double)6;
+//
+//    System.out.println("total = "+total);
+//    System.out.println("avg = "+avg);
+
+
+//    int[] scores = {100,90,80,90,78};
+//    int total = 0;
+//    for (int i = 0; i < scores.length; i++) {
+//      total += scores[0];
+//    }
+//    System.out.println(total);
+//    System.out.println(total/(scores.length+1));
+
+
+//    Random random = new Random();
+//    int[] scores = new int[7];
+//    for (int i = 0; i < scores.length; i++) {
+//      scores[i] = random.nextInt(0,100);
+//    }
+//    System.out.println(Arrays.toString(scores));
+//    char[] chars = {'a','b','c'};
+//    System.out.println(chars);
+//    System.out.println(scores);
+
+
+//    Scanner sc = new Scanner(System.in);
+//    int[] scores = new int[5];
+//    int total = 0;
+//    int avg;
+//
+//    for (int i = 0; i < scores.length; i++) {
+//      scores[i]=sc.nextInt();
+//    }
+//
+//    for (int i = 0; i <scores.length ; i++) {
+//      total +=scores[i];
+//    }
+//
+//    avg = total/scores.length;
+//    System.out.println(total+" "+avg);
+//    sc.nextInt();
+//  }
+
+
+//    ArrayList<Integer> scores = new ArrayList<>();
+//    scores.add(10);
+//    scores.add(20);
+//
+//    System.out.println(scores.toString());
+
+  }
+}
---
+++ src/ArrayListDemo.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ArrayListDemo {
+  public static void main(String[] args) {
+
+//    ArrayList<String> names = new ArrayList<>();
+//    names.add("kim");
+//    names.add("lee");
+//    names.add("park");
+//
+//    System.out.println(names.toString());
+//    System.out.println(names.size());
+//
+//    for (int i = 0; i < names.size(); i++) {
+//      String name =names.get(i);
+//      if (name != null && name.length() == 4) {
+//        names.remove(i);
+//        names.add(i, name.substring(1,name.length())) ;
+//      }
+//    }
+//    System.out.println(names.toString());
+    Scanner sc = new Scanner(System.in);
+    ArrayList<Integer> scores= new ArrayList<>();
+    int total = 0;
+    int avg,score = 0;
+
+    for (int i = 1; true; i++) {
+      System.out.println("점수를 입력하세요 (" + i + "번째) -1 입력시 종료");
+      score = sc.nextInt();
+      if (score == -1){
+       break;
+      }
+      scores.add(score);
+    }
+
+    for (int i = 0; i < scores.size(); i++) {
+      total += scores.get(i);
+    }
+    avg = total/scores.size();
+    System.out.println("scores:"+scores.toString());
+    System.out.println("총합:"+total);
+    System.out.println("평균:"+avg);
+  }
+}
