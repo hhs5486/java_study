8e1452e7c569af185a8e25e5e1dd1e347623523c
first_commit <hhs5486@naver.com>
2025-03-10 10:54:47 +0900

    변경

---
+++ src/chapter4_oop/sumDemo/main.java
@@ -0,0 +1,10 @@
+package chapter4_oop.sumDemo;
+
+public class main {
+  public static void main(String[] args) {
+    sum sum = new sum();
+    sum.sum(1,2,3,4);
+    int arr[] = {2,3};
+    sum.sum(1, arr);
+  }
+}
